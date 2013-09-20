8b58156259f163520fceab0f67ad5eb010846fb5
thatsIch <badhard@arcor.de>
2013-09-20 02:04:42 +0200

    Updated Logger and Javadoc

--- src/main/java/de/thatsich/core/opencv/EuclideanDistance.java
+++ src/main/java/de/thatsich/core/opencv/EuclideanDistance.java
@@ -4,6 +4,11 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
+/**
+ * Calculates the Euclidean Distance between 2 Mats
+ * @author Minh
+ *
+ */
 public class EuclideanDistance extends AMetric {
 
 	/**
@@ -11,29 +16,26 @@ public class EuclideanDistance extends AMetric {
 	 */
 	@Override
 	public double getDistance(Mat original, Mat compare) throws IllegalStateException {
-		
-		double result = 0;
-		
-		// Test same size
+
 		if (!(original.size().equals(compare.size()))) throw new IllegalStateException("Size of original and compare differ.");
+		this.log.info("Tested for same size.");
 		
-		// check if really grayscale
 		if (original.channels() != 1 && compare.channels() != 1) throw new IllegalStateException("Not GrayScale.");
-	
-		// new grayscale image of size to display the difference
+		this.log.info("Tested for GrayScale images");
+		
 		Mat diff = new Mat(original.size(), CvType.CV_8UC1);
 		Core.absdiff(original, compare, diff);
+		this.log.info("Creating diff mat.");
 		
-		// square each
 		Core.pow(diff, 2, diff);
-		
-		// sum all
-		result = Core.sumElems(diff).val[0];
+		this.log.info("Sqaure each element within.");
 	
-		// root it
-		result = Math.sqrt(result);
+		double result = Core.sumElems(diff).val[0];
+		this.log.info("Sum all elements together.");
 		
+		result = Math.sqrt(result);
+		this.log.info("Squareroot the sum.");
+				
 		return 0;
 	}
-
 }
