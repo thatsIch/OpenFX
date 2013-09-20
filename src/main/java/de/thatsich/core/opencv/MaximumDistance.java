1606f115890a4ca5ed820468486db98658bbea5b
thatsIch <badhard@arcor.de>
2013-09-20 02:01:45 +0200

    Updated Logger and Javadoc

--- src/main/java/de/thatsich/core/opencv/MaximumDistance.java
+++ src/main/java/de/thatsich/core/opencv/MaximumDistance.java
@@ -5,25 +5,38 @@ import org.opencv.core.Core.MinMaxLocResult;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
+import com.google.inject.Inject;
+
+import de.thatsich.core.Log;
+
+/**
+ * Metric which uses the maximum distance to distinguish two mats
+ * 
+ * @author Minh
+ *
+ */
 public class MaximumDistance extends AMetric {
 
+	@Inject private Log log;
+	
 	/**
 	 * ||a - b||_\infty = max(abs(a_i - b_i))
 	 */
 	@Override
 	public double getDistance(Mat original, Mat compare) {
 				
-		// Test same size
 		if (!(original.size().equals(compare.size()))) throw new IllegalStateException("Size of original and compare differ.");
+		this.log.info("Tested for same size.");
 		
-		// check if really grayscale
 		if (original.channels() != 1 && compare.channels() != 1) throw new IllegalStateException("Not GrayScale.");
-	
-		// new grayscale image of size to display the difference
+		this.log.info("Tested for GrayScale images");
+		
 		Mat diff = new Mat(original.size(), CvType.CV_8UC1);
 		Core.absdiff(original, compare, diff);
+		this.log.info("Creating diff mat.");
 		
 		MinMaxLocResult minMax = Core.minMaxLoc(diff);
+		this.log.info("Maximum found: " + minMax.maxVal);
 		
 		return minMax.maxVal;
 	}
