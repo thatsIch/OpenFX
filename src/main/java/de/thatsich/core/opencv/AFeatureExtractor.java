7279a3d2bb08ce0d897c5177bb11fcfe7ae49bc4
thatsIch <badhard@arcor.de>
2013-09-24 03:42:32 +0200

    Javadoc

--- src/main/java/de/thatsich/core/opencv/AFeatureExtractor.java
+++ src/main/java/de/thatsich/core/opencv/AFeatureExtractor.java
@@ -2,15 +2,15 @@ package de.thatsich.core.opencv;
 
 
 /**
+ * A Feature Extractor
  * 
  * @author Minh
  *
  */
 public abstract class AFeatureExtractor implements IFeatureExtractor {
-
 	
 	/**
-	 * 
+	 * gets the name of the feature extractor 
 	 */
 	@Override
 	public String getName() {
