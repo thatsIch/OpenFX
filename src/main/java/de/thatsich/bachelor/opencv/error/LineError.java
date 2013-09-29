54f24c55defdbf44cd48573900b1bf0f0b599d1b
thatsIch <badhard@arcor.de>
2013-09-29 20:11:25 +0200

    Fixed Error in LineError Calculation because Sin and Cos use Radians and not Angle

--- src/main/java/de/thatsich/bachelor/opencv/error/LineError.java
+++ src/main/java/de/thatsich/bachelor/opencv/error/LineError.java
@@ -1,7 +1,5 @@
 package de.thatsich.bachelor.opencv.error;
 
-import java.util.Random;
-
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
@@ -19,11 +17,11 @@ public class LineError extends AErrorGenerator implements IErrorGenerator {
 	
 	@Override
 	public Mat generateError(Mat in) {
-		final Random r = new Random();
-		final int length = (int) Math.round(r.nextGaussian() * this.STDDEV_LENTH + this.MEAN_LENGTH);
-		final int angle = (int) Math.round(r.nextInt() * 360) - 180;
-		final int offset_x = length * (int) Math.round(Math.cos(angle));
-		final int offset_y = length * (int) Math.round(Math.sin(angle));
+		final int length = (int) Math.round(Math.random() * this.STDDEV_LENTH + this.MEAN_LENGTH);
+		final double radian = (Math.random() * Math.PI) + Math.PI / 2;
+		
+		final int offset_x = (int) Math.round(Math.cos(radian) * length);
+		final int offset_y = (int) Math.round(Math.sin(radian) * length);
 		
 		final int width = in.width();
 		final int height = in.height();
@@ -31,13 +29,13 @@ public class LineError extends AErrorGenerator implements IErrorGenerator {
 		final int leftWidth = width - offset_x;
 		final int leftHeight = height - offset_y;
 		
-		final int x1 = r.nextInt() * leftWidth;
-		final int y1 = r.nextInt() * leftHeight;
+		final int x1 = (int) Math.round(Math.random() * leftWidth);
+		final int y1 = (int) Math.round(Math.random() * leftHeight);
 		final int x2 = x1 + offset_x;
 		final int y2 = y1 + offset_y;
 		
-		final Scalar color = new Scalar(r.nextInt() * 255);
-		final int thickness = r.nextInt() * (this.MAX_LINE_THICKNESS - MIN_LINE_THICKNESS) + MIN_LINE_THICKNESS;
+		final Scalar color = new Scalar((int) Math.round(Math.random() * 255));
+		final int thickness = (int) Math.round(Math.random() * (this.MAX_LINE_THICKNESS - MIN_LINE_THICKNESS) + MIN_LINE_THICKNESS);
 		
 		Core.line(in, new Point(x1, y1), new Point(x2, y2), color, thickness);
 		// circle
