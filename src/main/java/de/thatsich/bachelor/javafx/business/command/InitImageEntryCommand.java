7a4a3b18e92c2ee928f94893b248c0b4db837c1b
thatsIch <badhard@arcor.de>
2013-10-16 17:21:37 +0200

    Outsourced Initializing of ImageEntry to the Command Layer instead into the Model

---
+++ src/main/java/de/thatsich/bachelor/javafx/business/command/InitImageEntryCommand.java
@@ -0,0 +1,58 @@
+package de.thatsich.bachelor.javafx.business.command;
+
+import java.io.IOException;
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
+
+import com.google.inject.Inject;
+
+import de.thatsich.bachelor.javafx.business.model.entity.ImageEntry;
+import de.thatsich.core.javafx.Command;
+
+public class InitImageEntryCommand extends Command<List<ImageEntry>> {
+
+	private final ObjectProperty<Path> imageInputPath = new SimpleObjectProperty<Path>();
+	
+	@Inject
+	protected InitImageEntryCommand(EventHandler<WorkerStateEvent> handler, Path imageInputPath) {
+		super(handler);
+		this.imageInputPath.set(imageInputPath);
+	}
+
+	@Override
+	protected Task<List<ImageEntry>> createTask() {
+		// TODO Auto-generated method stub
+		return new Task<List<ImageEntry>>() {
+
+			@Override
+			protected List<ImageEntry> call() throws Exception {
+				final List<ImageEntry> result = new ArrayList<ImageEntry>();
+
+				final String GLOB_PATTERN = "*.{png,jpeg,jpg,jpe}";
+				
+				try (DirectoryStream<Path> stream = Files.newDirectoryStream(imageInputPath.get(), GLOB_PATTERN)) {
+					for (Path child : stream) {
+						result.add(new ImageEntry(child.toAbsolutePath()));
+						log.info("Added " + child + " with Attribute " + Files.probeContentType(child));
+					}
+				} catch (IOException | DirectoryIteratorException e) {
+					e.printStackTrace();
+				}
+				log.info("All OpenCV Supported Images added: " + result.size() + ".");
+				
+				return result;
+			}
+		};
+	}
+
+}
