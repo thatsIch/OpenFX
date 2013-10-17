8012dea6dae50d4c6cfedb489faacfabb814f9ea
thatsIch <badhard@arcor.de>
2013-10-17 15:11:14 +0200

    Use ListProperty

--- src/main/java/de/thatsich/bachelor/javafx/business/model/ErrorDatabase.java
+++ src/main/java/de/thatsich/bachelor/javafx/business/model/ErrorDatabase.java
@@ -3,11 +3,12 @@ package de.thatsich.bachelor.javafx.business.model;
 import java.nio.file.Path;
 
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ListProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.ObservableList;
-import javafx.scene.control.ChoiceBox;
+import javafx.collections.FXCollections;
 import de.thatsich.bachelor.javafx.business.model.entity.ErrorEntry;
 import de.thatsich.core.opencv.IErrorGenerator;
 
@@ -15,9 +16,9 @@ public class ErrorDatabase {
 	
 	// Properties
 	final private ObjectProperty<Path> errorEntryFolderPath = new SimpleObjectProperty<Path>();
-	final private ObjectProperty<ObservableList<IErrorGenerator>> errorGenerators = new ChoiceBox<IErrorGenerator>().itemsProperty();
+	final private ListProperty<IErrorGenerator> errorGenerators = new SimpleListProperty<IErrorGenerator>(FXCollections.<IErrorGenerator>observableArrayList());
 	final private ObjectProperty<IErrorGenerator> errorGenerator = new SimpleObjectProperty<IErrorGenerator>();
-	final private ObjectProperty<ObservableList<ErrorEntry>> errorEntries = new ChoiceBox<ErrorEntry>().itemsProperty();
+	final private ListProperty<ErrorEntry> errorEntries = new SimpleListProperty<ErrorEntry>(FXCollections.<ErrorEntry>observableArrayList());
 	final private ObjectProperty<ErrorEntry> errorEntry = new SimpleObjectProperty<ErrorEntry>();
 	final private IntegerProperty errorLoopCount = new SimpleIntegerProperty();
 	
@@ -25,9 +26,9 @@ public class ErrorDatabase {
 	// Property Implementation
 	// ==================================================
 	public ObjectProperty<Path> getErrorEntryFolderPath() { return this.errorEntryFolderPath; }
-	public ObjectProperty<ObservableList<IErrorGenerator>> getErrorGeneratorListProperty() { return this.errorGenerators; }
+	public ListProperty<IErrorGenerator> getErrorGeneratorListProperty() { return this.errorGenerators; }
 	public ObjectProperty<IErrorGenerator> getSelectedErrorGeneratorProperty() { return this.errorGenerator; }
-	public ObjectProperty<ObservableList<ErrorEntry>> getErrorEntryListProperty() { return this.errorEntries; }
+	public ListProperty<ErrorEntry> getErrorEntryListProperty() { return this.errorEntries; }
 	public ObjectProperty<ErrorEntry> getSelectedErrorEntryProperty() { return this.errorEntry; }
 	public IntegerProperty getErrorLoopCountProperty() { return this.errorLoopCount; }
 }
