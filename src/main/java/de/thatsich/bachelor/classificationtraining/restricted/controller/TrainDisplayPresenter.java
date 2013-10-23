930055c69010e923b715b833cd09f9176f30d19b
thatsIch <badhard@arcor.de>
2013-10-23 17:15:12 +0200

    Created TrainDisplayView.fxml

--- src/main/java/de/thatsich/bachelor/classificationtraining/restricted/controller/TrainDisplayPresenter.java
+++ src/main/java/de/thatsich/bachelor/classificationtraining/restricted/controller/TrainDisplayPresenter.java
@@ -9,7 +9,11 @@ import de.thatsich.core.javafx.AFXMLPresenter;
 
 public class TrainDisplayPresenter extends AFXMLPresenter {
 
-	@FXML private Label label;
+	@FXML private Label nodeLabelClassifierName;
+	@FXML private Label nodeLabelExtractorName;
+	@FXML private Label nodeLabelFrameSize;
+	@FXML private Label nodeLabelErrorName;
+	@FXML private Label nodeLabelID;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resource) {
