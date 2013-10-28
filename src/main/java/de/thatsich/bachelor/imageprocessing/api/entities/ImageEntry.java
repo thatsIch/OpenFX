eb73ed4cb88f115027e7132edcad4b30e28eb843
thatsIch <badhard@arcor.de>
2013-10-28 03:34:49 +0100

    Change: Reduced Dependency of ImageEntry since it could handle its own FileSystem Change: Using Command, Controller, Model, Service, View package name convention

--- src/main/java/de/thatsich/bachelor/imageprocessing/api/entities/ImageEntry.java
+++ src/main/java/de/thatsich/bachelor/imageprocessing/api/entities/ImageEntry.java
@@ -2,40 +2,22 @@ package de.thatsich.bachelor.imageprocessing.api.entities;
 
 import java.nio.file.Path;
 
-import javafx.scene.image.Image;
-
 import org.opencv.core.Mat;
 
-import de.thatsich.core.opencv.Images;
-
 public class ImageEntry {
-	private Mat imageMat;
-	private Path imagePath;
+	private final Mat imageMat;
+	private final Path imagePath;
 	
-	public ImageEntry(Path imagePath) {
-		this.imageMat = Images.toMat(imagePath);
-		this.imageMat = Images.toGrayScale(this.imageMat);
+	public ImageEntry(Path imagePath, Mat imageMat) {
+		this.imageMat = imageMat;
 		this.imagePath = imagePath;
 	}
 	
-	public ImageEntry(Mat imageMat, Path imagePath) {
-		this.imageMat = Images.toGrayScale(imageMat);
-		this.imagePath = imagePath;
-	}
-	
-	public void store() {
-		Images.store(this.imageMat, this.imagePath);
-	}
-	
-	public Image getImage() {
-		return Images.toImage(this.imageMat);
-	}
-	
-	public Mat getMat() {
+	public Mat getImageMat() {
 		return this.imageMat;
 	}
 	
-	public Path getPath() {
+	public Path getImagePath() {
 		return this.imagePath;
 	}
 	
