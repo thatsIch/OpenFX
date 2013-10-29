30e6ec261336e281149b3407299c1d9d7ba3ea30
thatsIch <badhard@arcor.de>
2013-10-29 17:42:47 +0100

    New: Interface for FeatureExtractors

---
+++ src/main/java/de/thatsich/bachelor/featureextraction/api/core/IFeatureExtractors.java
@@ -0,0 +1,16 @@
+package de.thatsich.bachelor.featureextraction.api.core;
+
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
+import de.thatsich.bachelor.featureextraction.restricted.command.extractor.IFeatureExtractor;
+
+public interface IFeatureExtractors {
+	public ListProperty<IFeatureExtractor> getFeatureExtractorsProperty();
+	public ObjectProperty<IFeatureExtractor> getSelectedFeatureExtractorProperty();
+	
+	// Getter
+	public IFeatureExtractor getSelectedFeatureExtractor();
+	
+	// Setter
+	public void setSelectedFeatureExtractor(IFeatureExtractor extractor);
+}
--- src/main/java/de/thatsich/bachelor/featureextraction/restricted/models/FeatureExtractors.java
+++ src/main/java/de/thatsich/bachelor/featureextraction/restricted/models/FeatureExtractors.java
@@ -5,9 +5,10 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
+import de.thatsich.bachelor.featureextraction.api.core.IFeatureExtractors;
 import de.thatsich.bachelor.featureextraction.restricted.command.extractor.IFeatureExtractor;
 
-public class FeatureExtractors {
+public class FeatureExtractors implements IFeatureExtractors {
 
 	// Properties
 	private final ListProperty<IFeatureExtractor> featureExtractors = new SimpleListProperty<IFeatureExtractor>(FXCollections.<IFeatureExtractor>observableArrayList());
@@ -16,4 +17,10 @@ public class FeatureExtractors {
 	// Property Getter
 	public ListProperty<IFeatureExtractor> getFeatureExtractorsProperty() { return this.featureExtractors; }
 	public ObjectProperty<IFeatureExtractor> getSelectedFeatureExtractorProperty() { return this.selectedFeatureExtractor; }
+	
+	// Getter
+	public IFeatureExtractor getSelectedFeatureExtractor() { return this.selectedFeatureExtractor.get(); }
+	
+	// Setter
+	public void setSelectedFeatureExtractor(IFeatureExtractor extractor) { this.selectedFeatureExtractor.set(extractor); }
 }
