e2a50f225d068dcb4a29fcd43dd424e05f1681f8
thatsIch <badhard@arcor.de>
2013-10-30 13:51:17 +0100

    Change: Usage of AWiringModule

--- src/main/java/de/thatsich/bachelor/prediction/api/guice/TestWiringModule.java
+++ src/main/java/de/thatsich/bachelor/prediction/api/guice/TestWiringModule.java
@@ -1,6 +1,5 @@
 package de.thatsich.bachelor.prediction.api.guice;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 
 import de.thatsich.bachelor.prediction.intern.model.BinaryPredictions;
@@ -11,6 +10,7 @@ import de.thatsich.bachelor.prediction.intern.view.BinaryPredictionSplitChannelV
 import de.thatsich.bachelor.prediction.intern.view.TestDisplayView;
 import de.thatsich.bachelor.prediction.intern.view.TestInputView;
 import de.thatsich.bachelor.prediction.intern.view.TestListView;
+import de.thatsich.core.guice.AWiringModule;
 
 
 /**
@@ -19,43 +19,37 @@ import de.thatsich.bachelor.prediction.intern.view.TestListView;
  * @author Minh
  *
  */
-public class TestWiringModule extends AbstractModule {
-
-	/**
-	 * AbstractModule Implementation
-	 * 
-	 * Wires up all the interfaces to their representation 
-	 * or implementation.
-	 */
+public class TestWiringModule extends AWiringModule {
 	@Override
-	protected void configure() {
+	protected void bindModule() {
 		super.bind(TestWiringModule.class).toInstance(this);
-
-		this.mapViews();
-		this.mapServices();
-		this.mapModels();
 	}
 
-	/*
-	 * ==================================================
-	 * Private Helper
-	 * ==================================================
-	 * used to map interfaces to implementations
-	 */
-	private void mapViews() {
+	@Override
+	protected void bindView() {
 		super.bind(TestDisplayView.class).in(Scopes.SINGLETON);
 		super.bind(TestInputView.class).in(Scopes.SINGLETON);
 		super.bind(TestListView.class).in(Scopes.SINGLETON);
 		super.bind(BinaryPredictionSplitChannelView.class).in(Scopes.SINGLETON);
 	}
-	
-	private void mapServices() {
-		super.bind(BinaryPredictionFileStorageService.class).in(Scopes.SINGLETON);
-		super.bind(BinaryPredictionConfigService.class).in(Scopes.SINGLETON);
+
+	@Override
+	protected void bindController() {
 	}
-	
-	private void mapModels() {
+
+	@Override
+	protected void bindCommand() {
+	}
+
+	@Override
+	protected void bindModel() {
 		super.bind(BinaryPredictions.class).in(Scopes.SINGLETON);
 		super.bind(PredictionState.class).in(Scopes.SINGLETON);
+	}
+
+	@Override
+	protected void bindService() {
+		super.bind(BinaryPredictionFileStorageService.class).in(Scopes.SINGLETON);
+		super.bind(BinaryPredictionConfigService.class).in(Scopes.SINGLETON);
 	}
 }
