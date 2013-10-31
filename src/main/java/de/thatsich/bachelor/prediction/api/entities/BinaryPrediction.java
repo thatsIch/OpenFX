f08dbe024eb83f65a09f3b4a5d0172e01d75e9e2
thatsIch <badhard@arcor.de>
2013-10-31 15:10:00 +0100

    Change: Extend BinaryPrediction to hold file information

--- src/main/java/de/thatsich/bachelor/prediction/api/entities/BinaryPrediction.java
+++ src/main/java/de/thatsich/bachelor/prediction/api/entities/BinaryPrediction.java
@@ -2,8 +2,12 @@ package de.thatsich.bachelor.prediction.api.entities;
 
 import java.nio.file.Path;
 
+import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.ReadOnlyStringWrapper;
 
 import org.opencv.core.Mat;
 
@@ -13,21 +17,41 @@ public class BinaryPrediction {
 	private final ReadOnlyObjectWrapper<Mat> withError = new ReadOnlyObjectWrapper<>();
 	private final ReadOnlyObjectWrapper<Mat> errorIndication = new ReadOnlyObjectWrapper<>();
 	private final ReadOnlyObjectWrapper<Mat> errorPrediction = new ReadOnlyObjectWrapper<>();
+	private final ReadOnlyStringWrapper classifierName = new ReadOnlyStringWrapper();
+	private final ReadOnlyStringWrapper extractorName = new ReadOnlyStringWrapper();
+	private final ReadOnlyIntegerWrapper frameSize = new ReadOnlyIntegerWrapper();
+	private final ReadOnlyStringWrapper errorClassName = new ReadOnlyStringWrapper();
+	private final ReadOnlyStringWrapper id = new ReadOnlyStringWrapper();
 	
 	public BinaryPrediction(
 			Path filePath,
 			Mat withError,
 			Mat errorIndication,
-			Mat errorPrediction) {
+			Mat errorPrediction,
+			String classifierName,
+			String extractorName,
+			int frameSize,
+			String errorClassName,
+			String id) {
 		this.filePath.set(filePath);
 		this.withError.set(withError);
 		this.errorIndication.set(errorIndication);
 		this.errorPrediction.set(errorPrediction);
+		this.classifierName.set(classifierName);
+		this.extractorName.set(extractorName);
+		this.frameSize.set(frameSize);
+		this.errorClassName.set(errorClassName);
+		this.id.set(id);
 	}
 
 	// Getters
 	public ReadOnlyObjectProperty<Path> getFilePathProperty() { return this.filePath.getReadOnlyProperty(); }
 	public ReadOnlyObjectProperty<Mat> getWithErrorProperty() { return this.withError.getReadOnlyProperty(); }
 	public ReadOnlyObjectProperty<Mat> getErrorIndicationProperty() { return this.errorIndication.getReadOnlyProperty(); }
-	public ReadOnlyObjectProperty<Mat> getErrorPredictionProperty() { return this.errorPrediction.getReadOnlyProperty(); }	
+	public ReadOnlyObjectProperty<Mat> getErrorPredictionProperty() { return this.errorPrediction.getReadOnlyProperty(); }
+	public ReadOnlyStringProperty getClassifierNameProperty() { return this.classifierName.getReadOnlyProperty(); }
+	public ReadOnlyStringProperty getExtractorNameProperty() { return this.extractorName.getReadOnlyProperty(); }
+	public ReadOnlyIntegerProperty getFrameSizeProperty() { return this.frameSize.getReadOnlyProperty(); }
+	public ReadOnlyStringProperty getErrorClassNameProperty() { return this.errorClassName.getReadOnlyProperty(); }
+	public ReadOnlyStringProperty getIDProperty() { return this.id.getReadOnlyProperty(); }
 }
