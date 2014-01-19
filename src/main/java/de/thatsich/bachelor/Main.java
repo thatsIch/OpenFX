1f77c998b1bc84c531da7f89eda788aa748bd3b0
thatsIch <badhard@arcor.de>
2014-01-19 18:23:03 +0100

    Change: Add required Dependencies for TestSmallMultiLayerPerceptron

--- src/main/java/de/thatsich/bachelor/Main.java
+++ src/main/java/de/thatsich/bachelor/Main.java
@@ -1,7 +1,11 @@
 package de.thatsich.bachelor;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+
+import org.apache.hama.ml.perception.MultiLayerPerceptron;
+import org.apache.hama.ml.perception.SmallMultiLayerPerceptron;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -19,78 +23,107 @@ import de.thatsich.core.guice.LoggerModule;
 import de.thatsich.core.guice.PostInitModule;
 import de.thatsich.core.opencv.OpenCVLoader;
 
+
 // TODO need Threshold
 // TODO bind erfolgsrate precision recall
 // ODO low priority - Random Feature Extractor
 // TODO CNBC
 // ODO halted - AANN
-// ODO halted - evtl in Commander Klassen aufteilen sodass Handler alle am selben Platz sind und dass die Controller weniger Responsibility haben
+// ODO halted - evtl in Commander Klassen aufteilen sodass Handler alle am
+// selben Platz sind und dass die Controller weniger Responsibility haben
 /**
  * Main Execution Class
+ * 
  * @author Tran Minh Do
  */
-public class Main extends GuiceApplication {
-	
+public class Main extends GuiceApplication
+{
+
 	/**
 	 * First instantiated view.
 	 */
 	@Inject
-	private DisplayView view;
+	private DisplayView	view;
 
 	/**
 	 * 
 	 * @param args
 	 */
-    public static void main(String[] args) {
-    	// Loading OpenCV Bindings
-        OpenCVLoader.loadLibrary();
-        
-        Main.launch(args);
-    }
+	public static void main( String[] args )
+	{
+		String modelPath = "/tmp/sampleModel-testWriteReadMLP.data";
+		double learningRate = 0.3;
+		double regularization = 0.0; // no regularization
+		double momentum = 0; // no momentum
+		String squashingFunctionName = "Sigmoid";
+		String costFunctionName = "SquaredError";
 
-    
+		int[] layerSizeArray = new int[] { 3, 2, 2, 3 };
+
+		MultiLayerPerceptron mlp = new SmallMultiLayerPerceptron(learningRate,
+	        regularization, momentum, squashingFunctionName, costFunctionName,
+	        layerSizeArray);
+		
+//		mlp.train( null, null )
+		
+		System.out.println("Finished");
+		
+		// Loading OpenCV Bindings
+//		OpenCVLoader.loadLibrary();
+
+//		Main.launch( args );
+	}
+
 	/*
-	 * ================================================== 
+	 * ==================================================
 	 * GuiceApplication Implementation
 	 * ==================================================
 	 */
 	/**
 	 * Initialize the Application with the first scene on stage.
 	 * Called automatically by Super Class.
-	 * @param primaryStage Main Stage to be drawn on
+	 * 
+	 * @param primaryStage
+	 *            Main Stage to be drawn on
 	 */
-    @Override public void start(Stage primaryStage) throws IOException {
-		if (this.view == null) throw new NullPointerException("View not instantiated."); 
-		
+	@Override
+	public void start( Stage primaryStage ) throws IOException
+	{
+		if ( this.view == null ) throw new NullPointerException( "View not instantiated." );
+
 		final Parent root = this.view.getRoot();
-		if (root == null) throw new NullPointerException("Root damaged.");
-		
-		final URL url = this.getClass().getResource("opencv.png");
-		if (url == null) throw new NullPointerException("Icon not found."); 
-		final Image icon = new Image(url.toString());
-		if (icon.isError()) throw new IllegalStateException("Icon could not be loaded");
-		
-		 // Put the loaded user interface onto the primary stage.
-		final Scene scene = new Scene(root);
-		
-        // Show the primary stage
+		if ( root == null ) throw new NullPointerException( "Root damaged." );
+
+		final URL url = this.getClass().getResource( "opencv.png" );
+		if ( url == null ) throw new NullPointerException( "Icon not found." );
+		final Image icon = new Image( url.toString() );
+		if ( icon.isError() ) throw new IllegalStateException( "Icon could not be loaded" );
+
+		// Put the loaded user interface onto the primary stage.
+		final Scene scene = new Scene( root );
+
+		// Show the primary stage
 		primaryStage.setTitle( "Sample" );
 		primaryStage.getIcons().add( icon );
 		primaryStage.setScene( scene );
 		primaryStage.setResizable( false );
 		primaryStage.show();
 	}
-	
+
 	/**
 	 * Initialize all added modules
 	 * Automatically called by super class
-	 * @param modules Empty Module-List 
+	 * 
+	 * @param modules
+	 *            Empty Module-List
 	 */
-	@Override public void init(List<Module> modules) {
-		modules.add(new LoggerModule());
-		modules.add(new PostInitModule());
-		
-		modules.add(new GridWiringModule());
-		modules.add(new GridCommandModule());
+	@Override
+	public void init( List<Module> modules )
+	{
+		modules.add( new LoggerModule() );
+		modules.add( new PostInitModule() );
+
+		modules.add( new GridWiringModule() );
+		modules.add( new GridCommandModule() );
 	}
 }
---
+++ src/test/java/org/apache/hama/ml/perception/TestSmallMultiLayerPerceptron.java
@@ -0,0 +1,460 @@
+package org.apache.hama.ml.perception;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.WritableUtils;
+import org.apache.hama.ml.math.DenseDoubleMatrix;
+import org.apache.hama.ml.math.DenseDoubleVector;
+import org.apache.hama.ml.math.DoubleMatrix;
+import org.apache.hama.ml.math.DoubleVector;
+import org.apache.hama.ml.writable.MatrixWritable;
+import org.apache.hama.ml.writable.VectorWritable;
+import org.junit.Test;
+
+
+public class TestSmallMultiLayerPerceptron
+{
+
+	/**
+	 * Write and read the parameters of MLP.
+	 */
+	@Test
+	public void testWriteReadMLP()
+	{
+		String modelPath = "/tmp/sampleModel-testWriteReadMLP.data";
+		double learningRate = 0.3;
+		double regularization = 0.0; // no regularization
+		double momentum = 0; // no momentum
+		String squashingFunctionName = "Sigmoid";
+		String costFunctionName = "SquaredError";
+		int[] layerSizeArray = new int[] { 3, 2, 2, 3 };
+		MultiLayerPerceptron mlp = new SmallMultiLayerPerceptron( learningRate, regularization, momentum, squashingFunctionName, costFunctionName, layerSizeArray );
+		try
+		{
+			mlp.writeModelToFile( modelPath );
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+		}
+
+		try
+		{
+			// read the meta-data
+			Configuration conf = new Configuration();
+			FileSystem fs = FileSystem.get( conf );
+			mlp = new SmallMultiLayerPerceptron( modelPath );
+			assertEquals( mlp.getClass().getName(), mlp.getMLPType() );
+			assertEquals( learningRate, mlp.getLearningRate(), 0.001 );
+			assertEquals( regularization, mlp.isRegularization(), 0.001 );
+			assertEquals( layerSizeArray.length, mlp.getNumberOfLayers() );
+			assertEquals( momentum, mlp.getMomentum(), 0.001 );
+			assertEquals( squashingFunctionName, mlp.getSquashingFunctionName() );
+			assertEquals( costFunctionName, mlp.getCostFunctionName() );
+			assertArrayEquals( layerSizeArray, mlp.getLayerSizeArray() );
+			// delete test file
+			fs.delete( new Path( modelPath ), true );
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test the output of an example MLP.
+	 */
+	@Test
+	public void testOutput()
+	{
+		// write the MLP meta-data manually
+		String modelPath = "/tmp/sampleModel-testOutput.data";
+		Configuration conf = new Configuration();
+		try
+		{
+			FileSystem fs = FileSystem.get( conf );
+			FSDataOutputStream output = fs.create( new Path( modelPath ), true );
+
+			String MLPType = SmallMultiLayerPerceptron.class.getName();
+			double learningRate = 0.5;
+			double regularization = 0.0;
+			double momentum = 0.1;
+			String squashingFunctionName = "Sigmoid";
+			String costFunctionName = "SquaredError";
+			int[] layerSizeArray = new int[] { 3, 2, 3, 3 };
+			int numberOfLayers = layerSizeArray.length;
+
+			WritableUtils.writeString( output, MLPType );
+			output.writeDouble( learningRate );
+			output.writeDouble( regularization );
+			output.writeDouble( momentum );
+			output.writeInt( numberOfLayers );
+			WritableUtils.writeString( output, squashingFunctionName );
+			WritableUtils.writeString( output, costFunctionName );
+
+			// write the number of neurons for each layer
+			for ( int i = 0; i < numberOfLayers; ++i )
+			{
+				output.writeInt( layerSizeArray[i] );
+			}
+
+			double[][] matrix01 = { // 4 by 2
+			{ 0.5, 0.2 }, { 0.1, 0.1 }, { 0.2, 0.5 }, { 0.1, 0.5 } };
+
+			double[][] matrix12 = { // 3 by 3
+			{ 0.1, 0.2, 0.5 }, { 0.2, 0.5, 0.2 }, { 0.5, 0.5, 0.1 } };
+
+			double[][] matrix23 = { // 4 by 3
+				{ 0.2, 0.5, 0.2 },
+				{ 0.5, 0.1, 0.5 },
+				{ 0.1, 0.2, 0.1 },
+				{ 0.1, 0.2, 0.5 } };
+
+			DoubleMatrix[] matrices = {
+				new DenseDoubleMatrix( matrix01 ),
+				new DenseDoubleMatrix( matrix12 ),
+				new DenseDoubleMatrix( matrix23 ) };
+			for ( DoubleMatrix mat : matrices )
+			{
+				MatrixWritable.write( mat, output );
+			}
+			output.close();
+
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+		}
+
+		// initial the mlp with existing model meta-data and get the output
+		MultiLayerPerceptron mlp = new SmallMultiLayerPerceptron( modelPath );
+		DoubleVector input = new DenseDoubleVector( new double[] { 1, 2, 3 } );
+		try
+		{
+			DoubleVector result = mlp.output( input );
+			assertArrayEquals( new double[] { 0.6636557, 0.7009963, 0.7213835 }, result.toArray(), 0.0001 );
+		}
+		catch ( Exception e1 )
+		{
+			e1.printStackTrace();
+		}
+
+		// delete meta-data
+		try
+		{
+			FileSystem fs = FileSystem.get( conf );
+			fs.delete( new Path( modelPath ), true );
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Test training with squared error on the XOR problem.
+	 */
+	@Test
+	public void testTrainWithSquaredError()
+	{
+		// generate training data
+		DoubleVector[] trainingData = new DenseDoubleVector[] {
+			new DenseDoubleVector( new double[] { 0, 0, 0 } ),
+			new DenseDoubleVector( new double[] { 0, 1, 1 } ),
+			new DenseDoubleVector( new double[] { 1, 0, 1 } ),
+			new DenseDoubleVector( new double[] { 1, 1, 0 } ) };
+
+		// set parameters
+		double learningRate = 0.3;
+		double regularization = 0.02; // no regularization
+		double momentum = 0; // no momentum
+		String squashingFunctionName = "Sigmoid";
+		String costFunctionName = "SquaredError";
+		int[] layerSizeArray = new int[] { 2, 5, 1 };
+		SmallMultiLayerPerceptron mlp = new SmallMultiLayerPerceptron( learningRate, regularization, momentum, squashingFunctionName, costFunctionName, layerSizeArray );
+
+		try
+		{
+			// train by multiple instances
+			Random rnd = new Random();
+			for ( int i = 0; i < 100000; ++i )
+			{
+				DenseDoubleMatrix[] weightUpdates = mlp.trainByInstance( trainingData[rnd.nextInt( 4 )] );
+				mlp.updateWeightMatrices( weightUpdates );
+			}
+
+			// System.out.printf("Weight matrices: %s\n",
+			// mlp.weightsToString(mlp.getWeightMatrices()));
+			for ( int i = 0; i < trainingData.length; ++i )
+			{
+				DenseDoubleVector testVec = ( DenseDoubleVector ) trainingData[i].slice( 2 );
+				double expected = trainingData[i].toArray()[2];
+				double actual = mlp.output( testVec ).toArray()[0];
+				if ( expected < 0.5 && actual >= 0.5 || expected >= 0.5 && actual < 0.5 )
+				{
+					System.out.println( "Neural network failes to lear the XOR." );
+				}
+			}
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test training with cross entropy on the XOR problem.
+	 */
+	@Test
+	public void testTrainWithCrossEntropy()
+	{
+		// generate training data
+		DoubleVector[] trainingData = new DenseDoubleVector[] {
+			new DenseDoubleVector( new double[] { 0, 0, 0 } ),
+			new DenseDoubleVector( new double[] { 0, 1, 1 } ),
+			new DenseDoubleVector( new double[] { 1, 0, 1 } ),
+			new DenseDoubleVector( new double[] { 1, 1, 0 } ) };
+
+		// set parameters
+		double learningRate = 0.3;
+		double regularization = 0.0; // no regularization
+		double momentum = 0; // no momentum
+		String squashingFunctionName = "Sigmoid";
+		String costFunctionName = "CrossEntropy";
+		int[] layerSizeArray = new int[] { 2, 7, 1 };
+		SmallMultiLayerPerceptron mlp = new SmallMultiLayerPerceptron( learningRate, regularization, momentum, squashingFunctionName, costFunctionName, layerSizeArray );
+
+		try
+		{
+			// train by multiple instances
+			Random rnd = new Random();
+			for ( int i = 0; i < 50000; ++i )
+			{
+				DenseDoubleMatrix[] weightUpdates = mlp.trainByInstance( trainingData[rnd.nextInt( 4 )] );
+				mlp.updateWeightMatrices( weightUpdates );
+			}
+
+			// System.out.printf("Weight matrices: %s\n",
+			// mlp.weightsToString(mlp.getWeightMatrices()));
+			for ( int i = 0; i < trainingData.length; ++i )
+			{
+				DenseDoubleVector testVec = ( DenseDoubleVector ) trainingData[i].slice( 2 );
+				double expected = trainingData[i].toArray()[2];
+				double actual = mlp.output( testVec ).toArray()[0];
+				if ( expected < 0.5 && actual >= 0.5 || expected >= 0.5 && actual < 0.5 )
+				{
+					System.out.println( "Neural network failes to lear the XOR." );
+				}
+			}
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test training with regularizatiion.
+	 */
+	@Test
+	public void testWithRegularization()
+	{
+		// generate training data
+		DoubleVector[] trainingData = new DenseDoubleVector[] {
+			new DenseDoubleVector( new double[] { 0, 0, 0 } ),
+			new DenseDoubleVector( new double[] { 0, 1, 1 } ),
+			new DenseDoubleVector( new double[] { 1, 0, 1 } ),
+			new DenseDoubleVector( new double[] { 1, 1, 0 } ) };
+
+		// set parameters
+		double learningRate = 0.3;
+		double regularization = 0.02; // regularization should be a tiny number
+		double momentum = 0; // no momentum
+		String squashingFunctionName = "Sigmoid";
+		String costFunctionName = "CrossEntropy";
+		int[] layerSizeArray = new int[] { 2, 7, 1 };
+		SmallMultiLayerPerceptron mlp = new SmallMultiLayerPerceptron( learningRate, regularization, momentum, squashingFunctionName, costFunctionName, layerSizeArray );
+
+		try
+		{
+			// train by multiple instances
+			Random rnd = new Random();
+			for ( int i = 0; i < 20000; ++i )
+			{
+				DenseDoubleMatrix[] weightUpdates = mlp.trainByInstance( trainingData[rnd.nextInt( 4 )] );
+				mlp.updateWeightMatrices( weightUpdates );
+			}
+
+			// System.out.printf("Weight matrices: %s\n",
+			// mlp.weightsToString(mlp.getWeightMatrices()));
+			for ( int i = 0; i < trainingData.length; ++i )
+			{
+				DenseDoubleVector testVec = ( DenseDoubleVector ) trainingData[i].slice( 2 );
+				double expected = trainingData[i].toArray()[2];
+				double actual = mlp.output( testVec ).toArray()[0];
+				if ( expected < 0.5 && actual >= 0.5 || expected >= 0.5 && actual < 0.5 )
+				{
+					System.out.println( "Neural network failes to lear the XOR." );
+				}
+			}
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test training with momentum. The MLP can converge faster.
+	 */
+	@Test
+	public void testWithMomentum()
+	{
+		// generate training data
+		DoubleVector[] trainingData = new DenseDoubleVector[] {
+			new DenseDoubleVector( new double[] { 0, 0, 0 } ),
+			new DenseDoubleVector( new double[] { 0, 1, 1 } ),
+			new DenseDoubleVector( new double[] { 1, 0, 1 } ),
+			new DenseDoubleVector( new double[] { 1, 1, 0 } ) };
+
+		// set parameters
+		double learningRate = 0.3;
+		double regularization = 0.02; // regularization should be a tiny number
+		double momentum = 0.5; // no momentum
+		String squashingFunctionName = "Sigmoid";
+		String costFunctionName = "CrossEntropy";
+		int[] layerSizeArray = new int[] { 2, 7, 1 };
+		SmallMultiLayerPerceptron mlp = new SmallMultiLayerPerceptron( learningRate, regularization, momentum, squashingFunctionName, costFunctionName, layerSizeArray );
+
+		try
+		{
+			// train by multiple instances
+			Random rnd = new Random();
+			for ( int i = 0; i < 5000; ++i )
+			{
+				DenseDoubleMatrix[] weightUpdates = mlp.trainByInstance( trainingData[rnd.nextInt( 4 )] );
+				mlp.updateWeightMatrices( weightUpdates );
+			}
+
+			// System.out.printf("Weight matrices: %s\n",
+			// mlp.weightsToString(mlp.getWeightMatrices()));
+			for ( int i = 0; i < trainingData.length; ++i )
+			{
+				DenseDoubleVector testVec = ( DenseDoubleVector ) trainingData[i].slice( 2 );
+				double expected = trainingData[i].toArray()[2];
+				double actual = mlp.output( testVec ).toArray()[0];
+				if ( expected < 0.5 && actual >= 0.5 || expected >= 0.5 && actual < 0.5 )
+				{
+					System.out.println( "Neural network failes to lear the XOR." );
+				}
+			}
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test the XOR problem.
+	 */
+	@Test
+	public void testTrainingByXOR()
+	{
+		// write in some training instances
+		Configuration conf = new Configuration();
+		String strDataPath = "/tmp/xor-training-by-xor";
+		Path dataPath = new Path( strDataPath );
+
+		// generate training data
+		DoubleVector[] trainingData = new DenseDoubleVector[] {
+			new DenseDoubleVector( new double[] { 0, 0, 0 } ),
+			new DenseDoubleVector( new double[] { 0, 1, 1 } ),
+			new DenseDoubleVector( new double[] { 1, 0, 1 } ),
+			new DenseDoubleVector( new double[] { 1, 1, 0 } ) };
+
+		try
+		{
+			URI uri = new URI( strDataPath );
+			FileSystem fs = FileSystem.get( uri, conf );
+			fs.delete( dataPath, true );
+			if ( !fs.exists( dataPath ) )
+			{
+				fs.createNewFile( dataPath );
+				SequenceFile.Writer writer = new SequenceFile.Writer( fs, conf, dataPath, LongWritable.class, VectorWritable.class );
+
+				for ( int i = 0; i < 1000; ++i )
+				{
+					VectorWritable vecWritable = new VectorWritable( trainingData[i % 4] );
+					writer.append( new LongWritable( i ), vecWritable );
+				}
+				writer.close();
+			}
+
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+
+		// begin training
+		String modelPath = "/tmp/xorModel-training-by-xor.data";
+		double learningRate = 0.6;
+		double regularization = 0.02; // no regularization
+		double momentum = 0.3; // no momentum
+		String squashingFunctionName = "Tanh";
+		String costFunctionName = "SquaredError";
+		int[] layerSizeArray = new int[] { 2, 5, 1 };
+		SmallMultiLayerPerceptron mlp = new SmallMultiLayerPerceptron( learningRate, regularization, momentum, squashingFunctionName, costFunctionName, layerSizeArray );
+
+		Map<String, String> trainingParams = new HashMap<String, String>();
+		trainingParams.put( "training.iteration", "2000" );
+		trainingParams.put( "training.mode", "minibatch.gradient.descent" );
+		trainingParams.put( "training.batch.size", "100" );
+		trainingParams.put( "tasks", "3" );
+		trainingParams.put( "modelPath", modelPath );
+
+		try
+		{
+			mlp.train( dataPath, trainingParams );
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+
+		// test the model
+		for ( int i = 0; i < trainingData.length; ++i )
+		{
+			DenseDoubleVector testVec = ( DenseDoubleVector ) trainingData[i].slice( 2 );
+			try
+			{
+				double expected = trainingData[i].toArray()[2];
+				double actual = mlp.output( testVec ).toArray()[0];
+				if ( expected < 0.5 && actual >= 0.5 || expected >= 0.5 && actual < 0.5 )
+				{
+					System.out.println( "Neural network failes to lear the XOR." );
+				}
+			}
+			catch ( Exception e )
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
