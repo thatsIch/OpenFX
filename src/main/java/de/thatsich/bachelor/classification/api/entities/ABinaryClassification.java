50e3860d91173693b7f954ddbe242582965b8d01
thatsIch <badhard@arcor.de>
2014-01-21 20:01:20 +0100

    New: Javadoc

--- src/main/java/de/thatsich/bachelor/classification/api/entities/ABinaryClassification.java
+++ src/main/java/de/thatsich/bachelor/classification/api/entities/ABinaryClassification.java
@@ -12,26 +12,65 @@ import com.google.inject.Inject;
 import de.thatsich.bachelor.classification.intern.command.classifier.BinaryClassifierConfiguration;
 import de.thatsich.core.Log;
 
-public abstract class ABinaryClassification implements IBinaryClassification {
-	
+
+public abstract class ABinaryClassification implements IBinaryClassification
+{
+
 	// Properties
-	private final ReadOnlyObjectWrapper<BinaryClassifierConfiguration> config = new ReadOnlyObjectWrapper<>();
-	
+	private final ReadOnlyObjectWrapper<BinaryClassifierConfiguration>	config	= new ReadOnlyObjectWrapper<>();
+
 	// Injects
-	@Inject protected Log log; 
-	
-	protected ABinaryClassification(BinaryClassifierConfiguration config) {
-		this.config.set(config);
+	@Inject
+	protected Log														log;
+
+	/**
+	 * CTOR
+	 * 
+	 * @param config
+	 *            Configuration of the BinaryClassifier (FilePath,
+	 *            ClassificaationName, ExtractorName, FrameSize, ErrorName, ID)
+	 */
+	protected ABinaryClassification( BinaryClassifierConfiguration config )
+	{
+		this.config.set( config );
 	}
-	
+
 	// Getter
-	@Override public String getName() { return this.getClass().getSimpleName(); }
-	
+	@Override
+	public String getName()
+	{
+		return this.getClass().getSimpleName();
+	}
+
 	// Property Getter
-	@Override public ReadOnlyObjectProperty<Path> getFilePathProperty() { return this.config.get().getFilePath(); }
-	@Override public ReadOnlyStringProperty getClassificationNameProperty() { return this.config.get().getClassificationName(); }
-	@Override public ReadOnlyStringProperty getExtractorNameProperty() { return this.config.get().getExtractorName(); }
-	@Override public ReadOnlyIntegerProperty getFrameSizeProperty() { return this.config.get().getFrameSize(); }
-	@Override public ReadOnlyStringProperty getErrorNameProperty() { return this.config.get().getErrorName(); }
-	@Override public ReadOnlyStringProperty getIdProperty() { return this.config.get().getId(); }
+	@Override
+	public ReadOnlyObjectProperty<Path> getFilePathProperty()
+	{
+		return this.config.get().getFilePath();
+	}
+	@Override
+	public ReadOnlyStringProperty getClassificationNameProperty()
+	{
+		return this.config.get().getClassificationName();
+	}
+	@Override
+	public ReadOnlyStringProperty getExtractorNameProperty()
+	{
+		return this.config.get().getExtractorName();
+	}
+	@Override
+	public ReadOnlyIntegerProperty getFrameSizeProperty()
+	{
+		return this.config.get().getFrameSize();
+	}
+	@Override
+	public ReadOnlyStringProperty getErrorNameProperty()
+	{
+		return this.config.get().getErrorName();
+	}
+	@Override
+	public ReadOnlyStringProperty getIdProperty()
+	{
+		return this.config.get().getId();
+	}
 }
--- src/main/java/de/thatsich/bachelor/classification/api/entities/IBinaryClassification.java
+++ src/main/java/de/thatsich/bachelor/classification/api/entities/IBinaryClassification.java
@@ -8,15 +8,91 @@ import javafx.beans.property.ReadOnlyStringProperty;
 
 import org.opencv.core.Mat;
 
-public interface IBinaryClassification {
-	public String getName();
-	public double predict(Mat image);
-	public void load(String fileName);
-	public void save(String fileName);
-	public ReadOnlyObjectProperty<Path> getFilePathProperty();
-	public ReadOnlyStringProperty getClassificationNameProperty();
-	public ReadOnlyStringProperty getExtractorNameProperty();
-	public ReadOnlyIntegerProperty getFrameSizeProperty();
-	public ReadOnlyStringProperty getErrorNameProperty();
-	public ReadOnlyStringProperty getIdProperty();
+
+/**
+ * Interface for defining what a BinaryClassification needs to fullfill.
+ * BinaryClassifications are the results of a BinaryClassifier (RF, SVM etc)
+ * With that you need to be able to save a calculated one, load a previously
+ * calculated one and use it to predict.
+ * 
+ * It offers additional access to BinaryClassificationConfiguration.
+ * 
+ * 
+ * @author thatsIch
+ */
+public interface IBinaryClassification
+{
+	/**
+	 * Gets the name of the BinaryClassification
+	 * 
+	 * @return Name of BinaryClassification
+	 */
+	String getName();
+	
+	/**
+	 * Uses an image (in Mat form) to predict
+	 * 
+	 * @param image predicted image
+	 * 
+	 * @return probability of image prediction
+	 */
+	double predict( Mat image );
+	
+	/**
+	 * Loads a BinaryClassification saved as file
+	 * 
+	 * @param fileName Name of BinaryClassification File
+	 */
+	void load( String fileName );
+	
+	/**
+	 * Saves a BinaryClassification to file
+	 * 
+	 * @param fileName Name of BinaryClassification File
+	 */
+	void save( String fileName );
+	
+	/**
+	 * Gets Property of FilePath
+	 * 
+	 * @return Property of FilePath
+	 */
+	ReadOnlyObjectProperty<Path> getFilePathProperty();
+
+	/**
+	 * Gets Property of FilePath
+	 * 
+	 * @return Property of FilePath
+	 */
+	ReadOnlyStringProperty getClassificationNameProperty();
+
+	/**
+	 * Gets Property of ExtractorName
+	 * 
+	 * @return Property of ExtractorName
+	 */
+	ReadOnlyStringProperty getExtractorNameProperty();
+
+	/**
+	 * Gets Property of FrameSize
+	 * 
+	 * @return Property of FrameSize
+	 */
+	ReadOnlyIntegerProperty getFrameSizeProperty();
+	
+
+	/**
+	 * Gets Property of ErrorName
+	 * 
+	 * @return Property of ErrorName
+	 */
+	ReadOnlyStringProperty getErrorNameProperty();
+	
+
+	/**
+	 * Gets Property of ID
+	 * 
+	 * @return Property of ID
+	 */
+	ReadOnlyStringProperty getIdProperty();
 }
