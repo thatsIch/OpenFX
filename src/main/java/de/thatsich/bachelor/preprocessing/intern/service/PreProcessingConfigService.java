f68a5895669b51448c10882ad6f693ace21e58dd
thatsIch <badhard@arcor.de>
2014-01-22 16:17:42 +0100

    Add: PreProcessing Config Service

---
+++ src/main/java/de/thatsich/bachelor/preprocessing/intern/service/PreProcessingConfigService.java
@@ -0,0 +1,8 @@
+package de.thatsich.bachelor.preprocessing.intern.service;
+
+import de.thatsich.core.AConfigurationService;
+
+public class PreProcessingConfigService extends AConfigurationService
+{
+
+}
