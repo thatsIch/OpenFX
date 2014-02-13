979275f2260a51dacec73609ee4d73d6302497ed
thatsIch <badhard@arcor.de>
2014-02-13 22:15:59 +0100

    Change: Hooked up Presenter

--- src/main/java/de/thatsich/bachelor/classification/intern/control/ClassificationDisplayPresenter.java
+++ src/main/java/de/thatsich/bachelor/classification/intern/control/ClassificationDisplayPresenter.java
@@ -12,51 +12,62 @@ import de.thatsich.bachelor.classification.api.models.IBinaryClassifications;
 import de.thatsich.bachelor.classification.intern.command.ClassificationInitCommander;
 import de.thatsich.core.javafx.AFXMLPresenter;
 
-public class ClassificationDisplayPresenter extends AFXMLPresenter {
+
+public class ClassificationDisplayPresenter extends AFXMLPresenter
+{
 
 	// Nodes
-	@FXML private Label nodeLabelClassificationName;
-	@FXML private Label nodeLabelExtractorName;
-	@FXML private Label nodeLabelFrameSize;
-	@FXML private Label nodeLabelErrorName;
-	@FXML private Label nodeLabelID;
-	
+	@FXML private Label						nodeLabelClassificationName;
+	@FXML private Label						nodeLabelExtractorName;
+	@FXML private Label						nodeLabelFrameSize;
+	@FXML private Label						nodeLabelErrorName;
+	@FXML private Label						nodeLabelID;
+
 	// Injects
-	@Inject private IBinaryClassifications binaryClassifications;
-	
-	@Inject ClassificationInitCommander initCommander;
-	
+	@Inject private IBinaryClassifications	binaryClassifications;
+
+	@Inject ClassificationInitCommander		initCommander;
+
 	@Override
-	protected void initComponents() {
-		
+	protected void initComponents()
+	{
+
 	}
 
 	@Override
-	protected void bindComponents() {
+	protected void bindComponents()
+	{
 		this.bindLabels();
 	}
 
 	/**
 	 * Binds all labels to the changing FeatureVector
 	 */
-	private void bindLabels() {
-		this.binaryClassifications.getSelectedBinaryClassificationProperty().addListener(new ChangeListener<IBinaryClassification>() {
-			@Override public void changed(ObservableValue<? extends IBinaryClassification> observable, IBinaryClassification oldValue, IBinaryClassification newValue) {
-				if (newValue != null) {
-					nodeLabelClassificationName.setText(newValue.getClassificationNameProperty().getValue().toString());
-					nodeLabelExtractorName.setText(newValue.getExtractorNameProperty().getValue().toString());
-					nodeLabelFrameSize.setText(newValue.getFrameSizeProperty().getValue().toString());
-					nodeLabelErrorName.setText(newValue.getErrorNameProperty().getValue().toString());
-					nodeLabelID.setText(newValue.getIdProperty().getValue().toString());
-				} else {
-					nodeLabelClassificationName.setText(null);
-					nodeLabelExtractorName.setText(null);
-					nodeLabelFrameSize.setText(null);
-					nodeLabelErrorName.setText(null);
-					nodeLabelID.setText(null);
+	private void bindLabels()
+	{
+		this.binaryClassifications.getSelectedBinaryClassificationProperty().addListener( new ChangeListener<IBinaryClassification>()
+		{
+			@Override
+			public void changed( ObservableValue<? extends IBinaryClassification> observable, IBinaryClassification oldValue, IBinaryClassification newValue )
+			{
+				if ( newValue != null )
+				{
+					nodeLabelClassificationName.setText( newValue.getClassificationNameProperty().getValue().toString() );
+					nodeLabelExtractorName.setText( newValue.getExtractorNameProperty().getValue().toString() );
+					nodeLabelFrameSize.setText( newValue.getFrameSizeProperty().getValue().toString() );
+					nodeLabelErrorName.setText( newValue.getErrorNameProperty().getValue().toString() );
+					nodeLabelID.setText( newValue.getIdProperty().getValue().toString() );
+				}
+				else
+				{
+					nodeLabelClassificationName.setText( null );
+					nodeLabelExtractorName.setText( null );
+					nodeLabelFrameSize.setText( null );
+					nodeLabelErrorName.setText( null );
+					nodeLabelID.setText( null );
 				}
 			}
-		});
-		this.log.info("Bound Labels to changing BinaryClassification.");
+		} );
+		this.log.info( "Bound Labels to changing BinaryClassification." );
 	}
 }
--- src/main/java/de/thatsich/bachelor/preprocessing/intern/control/PreProcessingDisplayPresenter.java
+++ src/main/java/de/thatsich/bachelor/preprocessing/intern/control/PreProcessingDisplayPresenter.java
@@ -1,16 +1,28 @@
 package de.thatsich.bachelor.preprocessing.intern.control;
 
+import com.google.inject.Inject;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import de.thatsich.bachelor.preprocessing.api.entities.IPreProcessing;
+import de.thatsich.bachelor.preprocessing.api.models.IPreProcessings;
+import de.thatsich.bachelor.preprocessing.intern.command.PreProcessingInitCommander;
 import de.thatsich.core.javafx.AFXMLPresenter;
+
 
 public class PreProcessingDisplayPresenter extends AFXMLPresenter
 {
 	// Nodes
-	@FXML private Label nodeLabelPreProcessingName;
-	@FXML private Label nodeLabelInputSize;
-	@FXML private Label nodeLabelOutputSize;
-	@FXML private Label nodeLabelID;
+	@FXML private Label					nodeLabelPreProcessingName;
+	@FXML private Label					nodeLabelInputSize;
+	@FXML private Label					nodeLabelOutputSize;
+	@FXML private Label					nodeLabelID;
+
+	// Injects
+	@Inject private IPreProcessings		preProcessings;
+	@Inject PreProcessingInitCommander	commander;
 
 	@Override
 	protected void initComponents()
@@ -20,6 +32,35 @@ public class PreProcessingDisplayPresenter extends AFXMLPresenter
 	@Override
 	protected void bindComponents()
 	{
+		this.bindLabels();
 	}
 
+	/**
+	 * Binds all labels to the changing preprocessing
+	 */
+	private void bindLabels()
+	{
+		this.preProcessings.getSelectedPreProcessingProperty().addListener( new ChangeListener<IPreProcessing>()
+		{
+			@Override
+			public void changed( ObservableValue<? extends IPreProcessing> observable, IPreProcessing oldValue, IPreProcessing newValue )
+			{
+				if ( newValue != null )
+				{
+					nodeLabelPreProcessingName.setText( newValue.getPreProcessingNameProperty().getValue().toString() );
+					nodeLabelInputSize.setText( newValue.getInputSizeProperty().getValue().toString() );
+					nodeLabelOutputSize.setText( newValue.getOutputSizeProperty().getValue().toString() );
+					nodeLabelID.setText( newValue.getIdProperty().getValue().toString() );
+				}
+				else
+				{
+					nodeLabelPreProcessingName.setText( null );
+					nodeLabelInputSize.setText( null );
+					nodeLabelOutputSize.setText( null );
+					nodeLabelID.setText( null );
+				}
+			}
+		} );
+		this.log.info( "Bound labels to changing PreProcessing." );
+	}
 }
