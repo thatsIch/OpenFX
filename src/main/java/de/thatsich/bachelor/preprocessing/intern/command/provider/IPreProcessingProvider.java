8253a87e2998b6bf182bae9d1c303367e7cb9c6b
thatsIch <badhard@arcor.de>
2014-02-13 22:39:03 +0100

    Fix: replace interface by concrete implementation for factory

--- src/main/java/de/thatsich/bachelor/preprocessing/intern/command/provider/IPreProcessingProvider.java
+++ src/main/java/de/thatsich/bachelor/preprocessing/intern/command/provider/IPreProcessingProvider.java
@@ -2,12 +2,12 @@ package de.thatsich.bachelor.preprocessing.intern.command.provider;
 
 import org.encog.neural.networks.BasicNetwork;
 
-import de.thatsich.bachelor.preprocessing.api.entities.IPreProcessing;
+import de.thatsich.bachelor.preprocessing.intern.command.preprocessing.AANNPreProcessing;
 import de.thatsich.bachelor.preprocessing.intern.command.preprocessor.core.PreProcessorConfiguration;
 import de.thatsich.core.guice.ICommandProvider;
 
 public interface IPreProcessingProvider extends ICommandProvider
 {
-	IPreProcessing createAANNPreProcessing( BasicNetwork rebuildNetwork, PreProcessorConfiguration config );
+	AANNPreProcessing createAANNPreProcessing( BasicNetwork rebuildNetwork, PreProcessorConfiguration config );
 }
 
