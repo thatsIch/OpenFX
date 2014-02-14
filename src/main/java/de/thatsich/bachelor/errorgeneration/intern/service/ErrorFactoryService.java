65c6db8507ed1be17051569855e25c3194720bd3
thatsIch <badhard@arcor.de>
2014-02-14 13:21:40 +0100

    Change: cleanup

--- src/main/java/de/thatsich/bachelor/errorgeneration/intern/service/ErrorFactoryService.java
+++ src/main/java/de/thatsich/bachelor/errorgeneration/intern/service/ErrorFactoryService.java
@@ -3,8 +3,6 @@ package de.thatsich.bachelor.errorgeneration.intern.service;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
