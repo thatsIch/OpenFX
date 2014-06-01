d9d8c7063a7cf7efa2cb0e99fafb7057e18288aa
thatsIch <badhard@arcor.de>
2014-06-01 17:06:07 +0200

    Temp NetworkListPresenter

--- src/main/java/de/thatsich/bachelor/network/intern/view/NetworkListView.java
+++ src/main/java/de/thatsich/bachelor/network/intern/view/NetworkListView.java
@@ -3,4 +3,7 @@ package de.thatsich.bachelor.network.intern.view;
 import de.thatsich.bachelor.network.api.core.INetworkListView;
 import de.thatsich.core.javafx.AFXMLView;
 
-public class NetworkListView extends AFXMLView implements INetworkListView {}
+public class NetworkListView extends AFXMLView implements INetworkListView
+{
+
+}
