2674d47fd22d65ea43cbac0de90e20f7bf2d2999
thatsIch <thatsIch@mail.de>
2016-03-13 13:14:14 +0100

    Fixed logic bug in precision recall calculation

--- src/main/java/de/thatsich/openfx/prediction/intern/control/entity/NetworkPrediction.java
+++ src/main/java/de/thatsich/openfx/prediction/intern/control/entity/NetworkPrediction.java
@@ -11,149 +11,135 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 
 
-public class NetworkPrediction implements INetworkPrediction
-{
-	// Properties
-	private final ReadOnlyObjectWrapper<Mat> modified = new ReadOnlyObjectWrapper<>();
-	private final ReadOnlyObjectWrapper<IError[][]> errors = new ReadOnlyObjectWrapper<>();
-	private final ReadOnlyObjectWrapper<String[][]> errorClasses = new ReadOnlyObjectWrapper<>();
-	private final ReadOnlyObjectWrapper<Double[][]> errorPredictionMat = new ReadOnlyObjectWrapper<>();
+public class NetworkPrediction implements INetworkPrediction {
+    private static final double PREDICTION_TRUE_THRESHOLD = 0.75;
 
-	private final ReadOnlyIntegerWrapper truePositive = new ReadOnlyIntegerWrapper();
-	private final ReadOnlyIntegerWrapper falsePositive = new ReadOnlyIntegerWrapper();
-	private final ReadOnlyIntegerWrapper trueNegative = new ReadOnlyIntegerWrapper();
-	private final ReadOnlyIntegerWrapper falseNegative = new ReadOnlyIntegerWrapper();
+    // Properties
+    private final ReadOnlyObjectWrapper<Mat> modified = new ReadOnlyObjectWrapper<>();
+    private final ReadOnlyObjectWrapper<IError[][]> errors = new ReadOnlyObjectWrapper<>();
+    private final ReadOnlyObjectWrapper<String[][]> errorClasses = new ReadOnlyObjectWrapper<>();
+    private final ReadOnlyObjectWrapper<Double[][]> errorPredictionMat = new ReadOnlyObjectWrapper<>();
 
-	private final NetworkPredictionConfig config;
+    private final ReadOnlyIntegerWrapper truePositive = new ReadOnlyIntegerWrapper();
+    private final ReadOnlyIntegerWrapper falsePositive = new ReadOnlyIntegerWrapper();
+    private final ReadOnlyIntegerWrapper trueNegative = new ReadOnlyIntegerWrapper();
+    private final ReadOnlyIntegerWrapper falseNegative = new ReadOnlyIntegerWrapper();
 
-	public NetworkPrediction(NetworkPredictionConfig config, Mat modified, IError[][] errorSplit, String[][] errorClasses, Double[][] errorPredictionMat)
-	{
-		this.config = config;
+    private final NetworkPredictionConfig config;
 
-		this.modified.set(modified);
-		this.errors.set(errorSplit);
-		this.errorClasses.set(errorClasses);
-		this.errorPredictionMat.set(errorPredictionMat);
+    public NetworkPrediction(NetworkPredictionConfig config, Mat modified, IError[][] errorSplit, String[][] errorClasses, Double[][] errorPredictionMat) {
+        this.config = config;
 
-		this.calcTrueFalsePositiveNegative(errorSplit, errorPredictionMat);
-	}
+        this.modified.set(modified);
+        this.errors.set(errorSplit);
+        this.errorClasses.set(errorClasses);
+        this.errorPredictionMat.set(errorPredictionMat);
 
-	private void calcTrueFalsePositiveNegative(IError[][] onlyError, Double[][] prediction)
-	{
-		int truePositive = 0;
-		int falsePositive = 0;
-		int trueNegative = 0;
-		int falseNegative = 0;
+        this.calcTrueFalsePositiveNegative(errorSplit, errorPredictionMat);
+    }
 
-		final int cols = onlyError.length;
-		final int rows = onlyError[0].length;
+    private void calcTrueFalsePositiveNegative(IError[][] onlyError, Double[][] predictions) {
+        // a false positive is an error in data reporting in which a test result improperly indicates presence of a condition
 
-		for (int col = 0; col < cols; col++)
-		{
-			for (int row = 0; row < rows; row++)
-			{
-				final Mat error = onlyError[col][row].errorProperty().get();
-				final double pred = prediction[col][row];
-				if (this.hasError(error))
-				{
-					if (pred > 0.75)
-					{
-						truePositive++;
-					}
-					else
-					{
-						trueNegative++;
-					}
-				}
-				else
-				{
-					if (pred > 0.75)
-					{
-						falsePositive++;
-					}
-					else
-					{
-						falseNegative++;
-					}
-				}
-			}
-		}
 
-		this.truePositive.set(truePositive);
-		this.trueNegative.set(trueNegative);
-		this.falsePositive.set(falsePositive);
-		this.falseNegative.set(falseNegative);
-	}
+        int truePositive = 0;
+        int falsePositive = 0;
+        int trueNegative = 0;
+        int falseNegative = 0;
 
-	private boolean hasError(Mat error)
-	{
-		return Core.sumElems(error).val[0] != 0;
-	}
+        final int cols = onlyError.length;
+        final int rows = onlyError[0].length;
 
-	// Property Getters
-	@Override
-	public ReadOnlyObjectProperty<Mat> modified()
-	{
-		return this.modified.getReadOnlyProperty();
-	}
+        for (int col = 0; col < cols; col++) {
+            for (int row = 0; row < rows; row++) {
+                final Mat error = onlyError[col][row].errorProperty().get();
+                final double prediction = predictions[col][row];
+                final boolean isPositive = prediction > PREDICTION_TRUE_THRESHOLD;
 
-	@Override
-	public ReadOnlyObjectProperty<IError[][]> errors()
-	{
-		return this.errors.getReadOnlyProperty();
-	}
+                if (this.hasError(error)) {
+                    if (isPositive) {
+                        truePositive++;
+                    } else {
+                        falseNegative++;
+                    }
+                } else {
+                    if (isPositive) {
+                        falsePositive++;
+                    } else {
+                        trueNegative++;
+                    }
+                }
+            }
+        }
 
-	@Override
-	public ReadOnlyObjectProperty<String[][]> errorClasses() { return this.errorClasses.getReadOnlyProperty();}
+        this.truePositive.set(truePositive);
+        this.trueNegative.set(trueNegative);
+        this.falsePositive.set(falsePositive);
+        this.falseNegative.set(falseNegative);
+    }
 
-	@Override
-	public ReadOnlyObjectProperty<Double[][]> errorPredictions() { return this.errorPredictionMat.getReadOnlyProperty();}
+    private boolean hasError(Mat error) {
+        return Core.sumElems(error).val[0] != 0;
+    }
 
-	@Override
-	public ReadOnlyStringProperty dateTime()
-	{
-		return this.config.dateTime;
-	}
+    // Property Getters
+    @Override
+    public ReadOnlyObjectProperty<Mat> modified() {
+        return this.modified.getReadOnlyProperty();
+    }
 
-	@Override
-	public ReadOnlyStringProperty predictedClassName()
-	{
-		return this.config.predictedClassName;
-	}
+    @Override
+    public ReadOnlyObjectProperty<IError[][]> errors() {
+        return this.errors.getReadOnlyProperty();
+    }
 
-	@Override
-	public ReadOnlyStringProperty id()
-	{
-		return this.config.id;
-	}
+    @Override
+    public ReadOnlyObjectProperty<String[][]> errorClasses() {
+        return this.errorClasses.getReadOnlyProperty();
+    }
 
-	@Override
-	public NetworkPredictionConfig getConfig()
-	{
-		return this.config;
-	}
+    @Override
+    public ReadOnlyObjectProperty<Double[][]> errorPredictions() {
+        return this.errorPredictionMat.getReadOnlyProperty();
+    }
 
-	@Override
-	public ReadOnlyIntegerProperty truePositive()
-	{
-		return this.truePositive.getReadOnlyProperty();
-	}
+    @Override
+    public ReadOnlyStringProperty dateTime() {
+        return this.config.dateTime;
+    }
 
-	@Override
-	public ReadOnlyIntegerProperty trueNegative()
-	{
-		return this.trueNegative.getReadOnlyProperty();
-	}
+    @Override
+    public ReadOnlyStringProperty predictedClassName() {
+        return this.config.predictedClassName;
+    }
 
-	@Override
-	public ReadOnlyIntegerProperty falsePositive()
-	{
-		return this.falsePositive.getReadOnlyProperty();
-	}
+    @Override
+    public ReadOnlyStringProperty id() {
+        return this.config.id;
+    }
 
-	@Override
-	public ReadOnlyIntegerProperty falseNegative()
-	{
-		return this.falseNegative.getReadOnlyProperty();
-	}
+    @Override
+    public NetworkPredictionConfig getConfig() {
+        return this.config;
+    }
+
+    @Override
+    public ReadOnlyIntegerProperty truePositive() {
+        return this.truePositive.getReadOnlyProperty();
+    }
+
+    @Override
+    public ReadOnlyIntegerProperty trueNegative() {
+        return this.trueNegative.getReadOnlyProperty();
+    }
+
+    @Override
+    public ReadOnlyIntegerProperty falsePositive() {
+        return this.falsePositive.getReadOnlyProperty();
+    }
+
+    @Override
+    public ReadOnlyIntegerProperty falseNegative() {
+        return this.falseNegative.getReadOnlyProperty();
+    }
 }
